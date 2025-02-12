58c588291d29265214d209838cca06211a44b69a
Angelique Dumond <zayne.melnick@softwareofamerica.com>
2025-02-11 20:27:32 -0500

    initial commit

---
+++ sbbackend/src/main/java/zmelnick/sbbackend/SbbackendApplication.java
@@ -0,0 +1,13 @@
+package zmelnick.sbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbackendApplication.class, args);
+	}
+
+}
---
+++ sbbackend/src/test/java/zmelnick/sbbackend/SbbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package zmelnick.sbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
